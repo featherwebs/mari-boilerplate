8b16c5024edccd84e7b567cf65faea5f405704f6
Srawan Shrestha <shrestha.srawank@gmail.com>
2018-05-07 12:29:53 +0545

    updated event listeners

--- resources/assets/js/components/ImageSelector.vue
+++ resources/assets/js/components/ImageSelector.vue
@@ -1,35 +1,52 @@
 <template>
 	<div>
-		<div>
-			<div class="thumbnail">
-				<img :id="'holder-'+id" :src="value ? value : 'http://via.placeholder.com/250x250'" :data-input="'thumbnail-'+id" :data-preview="'holder-'+id">
+		<div v-if="type== 'file'" class="thumbnail" :id="'holder-file-wrapper-'+id" :data-input="'thumbnail-'+id" :data-preview="'holder-'+id">
+			<i :id="'holder-file-'+id" class="fa fa-file-o fa-5x" v-if="type == 'file'"></i>
+		</div>
+		<div v-else class="thumbnail" :id="'holder-wrapper-'+id" :data-input="'thumbnail-'+id" :data-preview="'holder-'+id">
+			<img :id="'holder-'+id" :src="img">
+		</div>
+		<div class="input-group">
+			<div class="input-group-btn">
+				<a v-if="type == 'file'" :id="'btn-file-'+id" :data-input="'thumbnail-'+id" :data-preview="'holder-'+id" class="btn btn-primary">
+					<i class="fa fa-file-o"></i> Choose
+				</a>
+				<a v-else :id="'btn-'+id" :data-input="'thumbnail-'+id" :data-preview="'holder-'+id"
+				   class="btn btn-primary">
+					<i class="fa fa-picture-o"></i> Choose
+				</a>
 			</div>
-			<a v-if="type == 'file'" :id="'btn-file-'+id" :data-input="'thumbnail-'+id" :data-preview="'holder-'+id" class="btn btn-primary">
-				<i class="fa fa-file-o"></i> Choose
-			</a>
-			<a v-else :id="'btn-'+id" :data-input="'thumbnail-'+id" :data-preview="'holder-'+id"
-			   class="btn btn-primary">
-				<i class="fa fa-picture-o"></i> Choose
-			</a>
-			<input :id="'thumbnail-'+id" class="form-control" type="hidden" :name="name" :value="value">
+			<input :id="'thumbnail-'+id" class="form-control"
+				   :type="hidevalue === false ? 'text': 'hidden'"
+				   :name="name"
+				   :value="img">
 		</div>
 	</div>
 </template>
 
 <script>
     export default {
-        props: ['name', 'value', 'type'],
+        props: ['name', 'value', 'type', 'hidevalue'],
         data() {
             return {
-                id: Math.random().toString(36).substring(8)
+                id: Math.random().toString(36).substring(8),
+                def: 'http://via.placeholder.com/250x250?text=x',
+                img: ''
             };
         },
         mounted() {
+            if (typeof this.value == 'string') {
+                this.img = this.value;
+            } else if (this.value && typeof this.value == 'object' && this.value.path) {
+                this.img = this.value.url;
+            } else if (this.type != 'file') {
+                this.img = this.def;
+            }
             $('#btn-' + this.id).filemanager('image');
             $('#btn-file-' + this.id).filemanager('file');
-            $('#holder-' + this.id).filemanager('image');
+            $('#holder-wrapper-' + this.id).filemanager('image');
+            $('#holder-file-wrapper-' + this.id).filemanager('file');
         },
-        methods: {
-        }
+        methods: {}
     }
 </script>
